6fa3d0b5b06ef7c451b9c755061d7fc2c061c34d
Aditya6571 <adityagiri657@yahoo.in>
2020-10-14 16:32:46 +0530

    Updates

---
+++ precache-manifest.1979174cc5ad958defebda7f0333aab0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ecfd8052a00ecdba6cb7465046f60e",
+    "url": "/Burger/index.html"
+  },
+  {
+    "revision": "f0b309f123b762fb34f9",
+    "url": "/Burger/static/css/main.6d5a0d07.chunk.css"
+  },
+  {
+    "revision": "9c88e25b0816e29eeff2",
+    "url": "/Burger/static/js/2.d28a6a36.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger/static/js/2.d28a6a36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0b309f123b762fb34f9",
+    "url": "/Burger/static/js/main.9c293ea3.chunk.js"
+  },
+  {
+    "revision": "5feff121260f15e6ad86",
+    "url": "/Burger/static/js/runtime-main.fd322fda.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
